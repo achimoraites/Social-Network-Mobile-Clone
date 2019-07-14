93f280fd5e7cb77b3d581cc49ab8fe0aa4e9b86b
Achilles Moraites <achimoraites@yahoo.gr>
2019-07-14 19:06:38 +0300

    added redux support

--- App.js
+++ App.js
@@ -2,12 +2,19 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Header from './src/components/Header';
 import PhotoFeed from './src/components/PhotoFeed';
+// redux
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './src/reducers';
+
 export default function App() {
   return (
-    <View>
-      <Header text="Photos" />
-      <PhotoFeed />
-    </View>
+    <Provider store={createStore(reducers)}>
+      <View>
+        <Header text="Photos" />
+        <PhotoFeed />
+      </View>
+    </Provider>
   );
 }
 
