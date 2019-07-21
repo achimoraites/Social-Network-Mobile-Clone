7bf532f97de4f437854a3714603b8e039d111bfe
Achilles Moraites <achimoraites@yahoo.gr>
2019-07-21 18:40:19 +0300

    user profile info

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { Card, Avatar, ListItem } from 'react-native-elements';
+import Header from '../components/Header';
 
 export default class ProfileScreen extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+        };
+    }
 
-  render() {
-    return (
-      <View>
-        <Text> ProfileScreen </Text>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View>
+                <Header text="Profile" />
+                <Card>
+                    <ListItem
+                        leftAvatar={{
+                            title: 'BP',
+                            size: 'large',
+                            source: { uri:  'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg' },
+                            showEditButton: false,
+                        }}
+                        title={'Beatrice Peterson'}
+                        subtitle={'Fulltime Stalker'}
+                        chevron
+                    />
+                </Card>
+            </View>
+        );
+    }
 }
